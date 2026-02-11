6bf23c3dbce75728c1ffca0bf9b5ad7939f3dbf3
aiddao <153690256+aiddao@users.noreply.github.com>
2026-02-11 21:01:36 +0000

    Added all logs and alerts

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hello, World!");
+console.error("This is my first JavaScript error!");
+alert("Aiden");
+alert("Paul");
+alert("Andrew L");
+alert("Harrison");
+alert("Andrew K");
+console.log(console);
